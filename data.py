e9e11a9de0150bf78e89701c0ef2651af87e26c5
Jalle Willems <01092004.jw@gmail.com>
2020-10-28 15:40:33 +0100

    Formatting and sensible variable names.

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ import requests, json, datetime
 
 # Retrieve the history data from our chosen API.
 def get_history(base_url, api_key, coin="BTC", base_currency="USD", granularity="day", timescale="1825"):
-
+    
     # Build the correct URL from given parameters.
     url = (base_url + "data/v2/histo" + granularity + "?fsym=" + coin + "&tsym="
         + base_currency + "&limit=" + timescale + "&api_key=" + api_key)
@@ -17,24 +17,39 @@ def get_history(base_url, api_key, coin="BTC", base_currency="USD", granularity=
     return response
 
 # Clean the history data by filtering out unneeded parts.
-def clean_data(history_data):
+def clean_history(data, trim=False):
+    stop_trim = False
+    start_index = 0
 
     # Loop through the data in the seccond "Data" field.
-    for day in history_data["Data"]["Data"]:
+    for index, entry in enumerate(data["Data"]["Data"]):
 
         # Change date info from UNIX timestamp to readable date format.
-        day['date'] = (datetime.datetime.fromtimestamp(day['time'])
+        entry['date'] = (datetime.datetime.fromtimestamp(entry['time'])
             .strftime('%Y-%m-%d'))
 
-        # Add an "average" field to every day of the history data.
-        day['average'] = (day['high'] + day['low']) / 2
+        # Add an "average" field to every entry of the history data.
+        entry['average'] = (entry['high'] + entry['low']) / 2
 
         # Delete unnecessary info including "time" field, we replaced it with "date".
-        del day['time']
-        del day["volumefrom"]
-        del day["volumeto"]
-        del day["conversionType"]
-        del day["conversionSymbol"]
+        del entry['time']
+        del entry["volumefrom"]
+        del entry["volumeto"]
+        del entry["conversionType"]
+        del entry["conversionSymbol"]
 
-    # Only return the usefull data from the seccond "Data" field.
-    return history_data['Data']['Data']
+        # If trimming is turned on, set startindex to the index of the first nonzero value.
+        if trim:
+            if entry['high'] == 0 and not stop_trim:
+                start_index = index + 1
+            else:
+                stop_trim = True
+
+    if trim:
+        # Return the data from the first nonzero point on.
+        return data['Data']['Data'][start_index:]
+        
+    # Only return the usefull history data from the seccond "Data" field.
+    return data['Data']['Data']
+
+    
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import data, json, sys
+import data, json, sys, graph
 
 
 # Open the keyfile to the read private API key.
@@ -9,16 +9,18 @@ with open('key.txt', 'r') as keyfile:
 base_url = "https://min-api.cryptocompare.com/"
 
 if __name__ == "__main__":
-    history_data = data.get_history(base_url, api_key)
+    history_data = data.get_history(base_url, api_key, coin="ETH")
 
     # If the retrieved data isn't a dictionary, it is our custom error message.
     # Valid data here should always be json which is represented in a dictionary.
     if type(history_data) != dict:
         sys.exit(history_data)
 
-    history_data = data.clean_data(history_data)
+    history_data = data.clean_history(history_data, trim=True)
+
+    graph.draw_from_json(history_data)
 
     # Write our organised and cleaned history data to a file.
-    with open("btc_hist.json", "w") as btc_hist:
+    with open("coin_hist.json", "w") as btc_hist:
         json.dump(history_data, btc_hist, indent=2)
         btc_hist.close()
